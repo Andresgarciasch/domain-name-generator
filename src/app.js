abaaaa65823aa3cb59b20c710cb49030c94f2566
andres garcia01~ <andres_28garcia@outlook.es>
2022-04-24 03:02:31 -0400

    domain-nameG

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Rigo from console");
 };
+//write your code here
+let pre = ["the", "our", "your", "his", "her"];
+
+let adj = ["incredible", "greatest", "amazing"];
+
+let noun = ["4geeksters", "table", "sweater", "robbedspareparts"];
+
+let post = [".com", ".net", ".us", ".hacked"];
+
+for (let i = 0; i < pre.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < post.length; l++) {
+        console.log(`${pre[i]}${adj[j]}${noun[k]}${post[l]}`);
+      }
+    }
+  }
+}
