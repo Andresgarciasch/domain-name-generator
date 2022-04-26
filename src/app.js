b3f8ffcaad8a6b5f0ee0cfc2f01c2816ed047e66
andres garcia01~ <andres_28garcia@outlook.es>
2022-04-26 17:23:45 -0400

    last-commit

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ window.onload = function() {
   console.log("Hello Rigo from console");
 };
 //write your code here
-let pre = ["the", "our", "your", "his", "her"];
+let pre = ["the", "our", "your", "his_", "her_"];
 
 let adj = ["incredible", "greatest", "amazing"];
 
@@ -17,12 +17,18 @@ let noun = ["4geeksters", "table", "sweater", "robbedspareparts"];
 
 let post = [".com", ".net", ".us", ".hacked"];
 
-for (let i = 0; i < pre.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
-      for (let l = 0; l < post.length; l++) {
-        console.log(`${pre[i]}${adj[j]}${noun[k]}${post[l]}`);
+let result = "";
+
+window.onload = function() {
+  for (let i = 0; i < pre.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < post.length; l++) {
+          result += `${pre[i]}${adj[j]}${noun[k]}${post[l]}<br>`;
+          let element = document.getElementById("excuse");
+          element.innerHTML = result;
+        }
       }
     }
   }
-}
+};
